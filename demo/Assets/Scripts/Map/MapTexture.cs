32971e8389e798874b2c1f4f71884274701d5301
ivan.z <losetear@gmail.com>
2015-06-23 22:34:42 +0800

    优化河流渲染

--- demo/Assets/Scripts/Map/MapTexture.cs
+++ demo/Assets/Scripts/Map/MapTexture.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using UnityEngine;
 
 namespace Assets.Map
 {
     public class MapTexture
     {
-        private int _textureScale;
+        private readonly int _textureScale;
 
         public MapTexture(int textureScale)
         {
@@ -17,25 +15,11 @@ namespace Assets.Map
 
         public void AttachTexture(GameObject plane, Map map, NoisyEdges noisyEdge)
         {
-            int _textureWidth = (int) Map.Width*_textureScale;
-            int _textureHeight = (int) Map.Height*_textureScale;
+            int textureWidth = (int) Map.Width*_textureScale;
+            int textureHeight = (int) Map.Height*_textureScale;
 
-            Texture2D texture = new Texture2D(_textureWidth, _textureHeight,TextureFormat.RGB565, true);
-            texture.SetPixels(Enumerable.Repeat(BiomeProperties.Colors[Biome.Ocean], _textureWidth * _textureHeight).ToArray());
-
-//            var lines = map.Graph.edges.Where(p => p.v0 != null).Select(p => new[]
-//            {
-//                p.v0.point.x, p.v0.point.y,
-//                p.v1.point.x, p.v1.point.y
-//            }).ToArray();
-//
-//            foreach (var line in lines)
-//                DrawLine(texture, line[0], line[1], line[2], line[3], Color.black);
-
-//            foreach (var c in map.Graph.centers)
-//                texture.FillPolygon(
-//                    c.corners.Select(p => new Vector2(p.point.x*_textureScale, p.point.y*_textureScale)).ToArray(),
-//                    BiomeProperties.Colors[c.biome]);
+            Texture2D texture = new Texture2D(textureWidth, textureHeight,TextureFormat.RGB565, true);
+            texture.SetPixels(Enumerable.Repeat(BiomeProperties.Colors[Biome.Ocean], textureWidth * textureHeight).ToArray());
 
             //绘制扰乱的边缘
             foreach (Center p in map.Graph.centers)
@@ -53,34 +37,34 @@ namespace Assets.Map
                     //绘制扰乱后的形状
                     DrawNoisyPolygon(texture, p, noisyEdge.path0[edge.index]);
                     DrawNoisyPolygon(texture, p, noisyEdge.path1[edge.index]);
-                    //绘制扰乱后的边缘
-//                    List<Vector2> edge0 = noisyEdge.path0[edge.index];
-//                    for (int i = 0; i < edge0.Count - 1; i++)
-//                        DrawLine(texture, edge0[i].x, edge0[i].y, edge0[i + 1].x, edge0[i + 1].y, Color.red);
-//
-//                    List<Vector2> edge1 = noisyEdge.path1[edge.index];
-//                    for (int i = 0; i < edge1.Count - 1; i++)
-//                        DrawLine(texture, edge1[i].x, edge1[i].y, edge1[i + 1].x, edge1[i + 1].y, Color.red);
                 }
             }
-
+            //绘制扰乱后的河流
             foreach (var line in map.Graph.edges.Where(p => p.river > 0 && !p.d0.water && !p.d1.water))
-                DrawLine(texture, line.v0.point.x, line.v0.point.y, line.v1.point.x, line.v1.point.y, Color.blue);
+            {
+                //绘制扰乱后的边缘
+                List<Vector2> edge0 = noisyEdge.path0[line.index];
+                for (int i = 0; i < edge0.Count - 1; i++)
+                    DrawLine(texture, edge0[i].x, edge0[i].y, edge0[i + 1].x, edge0[i + 1].y, Color.blue);
+
+                List<Vector2> edge1 = noisyEdge.path1[line.index];
+                for (int i = 0; i < edge1.Count - 1; i++)
+                    DrawLine(texture, edge1[i].x, edge1[i].y, edge1[i + 1].x, edge1[i + 1].y, Color.blue);
+            }
 
             texture.Apply();
 
             plane.GetComponent<Renderer>().material.mainTexture = texture;
-            //plane.transform.localPosition = new Vector3(Map.Width/2, Map.Height/2, 1);
         }
 
-        List<Vector2> edgePoints = new List<Vector2>();
+        readonly List<Vector2> _edgePoints = new List<Vector2>();
         private void DrawNoisyPolygon(Texture2D texture, Center p, List<Vector2> orgEdges)
         {
-            edgePoints.Clear();
-            edgePoints.AddRange(orgEdges);
-            edgePoints.Add(p.point);
+            _edgePoints.Clear();
+            _edgePoints.AddRange(orgEdges);
+            _edgePoints.Add(p.point);
             texture.FillPolygon(
-                edgePoints.Select(x => new Vector2(x.x * _textureScale, x.y * _textureScale)).ToArray(),
+                _edgePoints.Select(x => new Vector2(x.x * _textureScale, x.y * _textureScale)).ToArray(),
                 BiomeProperties.Colors[p.biome]);
         }
 
