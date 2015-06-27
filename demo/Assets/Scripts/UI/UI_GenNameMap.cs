a0236f3b357bcc34d6104aebfaef590adae067a2
ivan <losetear@gmail.com>
2015-06-27 14:30:37 +0800

    添加渲染参数

--- demo/Assets/Scripts/UI/UI_GenNameMap.cs
+++ demo/Assets/Scripts/UI/UI_GenNameMap.cs
@@ -24,22 +24,62 @@ public class UI_GenNameMap : MonoBehaviour
         _btnGen.onClick.AddListener(GenMap);
 
 	    _showMap = GameObject.Find("Map");
+
+        transform.FindChild("Toggles1/Toggle1").GetComponent<Toggle>().onValueChanged.AddListener(Toggle1);
+        transform.FindChild("Toggles1/Toggle2").GetComponent<Toggle>().onValueChanged.AddListener(Toggle2);
+        transform.FindChild("Toggles1/Toggle3").GetComponent<Toggle>().onValueChanged.AddListener(Toggle3);
+        transform.FindChild("Toggles1/Toggle4").GetComponent<Toggle>().onValueChanged.AddListener(Toggle4);
+
+        transform.FindChild("Toggles2/Toggle1").GetComponent<Toggle>().onValueChanged.AddListener(ToggleLand);
+        transform.FindChild("Toggles2/Toggle2").GetComponent<Toggle>().onValueChanged.AddListener(ToggleLake);
 	}
 
     private static Texture2D _txtTexture;
     const int TextureScale = 20;
     private const int Width = 100;
     private const int Height = 50;
+    private int _pointNum = 1000;
+    private static bool _isLake;
+    void Toggle1(bool check)
+    {
+        if (check)
+            _pointNum = 1000;
+    }
+    void Toggle2(bool check)
+    {
+        if (check)
+            _pointNum = 2500;
+    }
+    void Toggle3(bool check)
+    {
+        if (check)
+            _pointNum = 4000;
+    }
+    void Toggle4(bool check)
+    {
+        if (check)
+            _pointNum = 10000;
+    }
+    void ToggleLand(bool check)
+    {
+        if (check)
+            _isLake = false;
+    }
+    void ToggleLake(bool check)
+    {
+        if (check)
+            _isLake = true;
+    }
+
     private void GenMap()
     {
-        //Random.seed = 1;
-
+        Random.seed = 1;
         _txtTexture = GetTextTexture();
 
         Map.Width = Width;
         Map.Height = Height;
         Map map = new Map();
-        map.SetPointNum(2500);
+        map.SetPointNum(_pointNum);
         map.Init(CheckIsland());
         //扰乱边缘
         NoisyEdges noisyEdge = new NoisyEdges();
@@ -54,7 +94,12 @@ public class UI_GenNameMap : MonoBehaviour
             int x = Convert.ToInt32(q.x / Width * _txtWidth);
             int y = Convert.ToInt32(q.y / Height * _txtHeight);
             Color tColor = _txtTexture.GetPixel(x,y);
-            return tColor == Color.white;
+            bool isLand = false;
+            if (_isLake)
+                isLand = tColor != Color.white;
+            else
+                isLand = tColor == Color.white;
+            return isLand;
         };
         return inside;
     }
@@ -77,7 +122,7 @@ public class UI_GenNameMap : MonoBehaviour
         gText.anchor = TextAnchor.MiddleCenter;
         gText.alignment = TextAlignment.Center;
         gText.font = _dFont;
-        gText.fontSize = 125;
+        gText.fontSize = gText.text.Length <=3?125: 100;
 
         _showMap.SetActive(false);
         myCamera.Render();
